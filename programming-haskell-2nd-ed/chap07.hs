dac9684636ec9eaf399b30d19223c77e24436f3d
koplec <koplec@github.com>
2024-05-06 01:02:19 +0900

    練習問題を解いている

--- programming-haskell-2nd-ed/chap07.hs
+++ programming-haskell-2nd-ed/chap07.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import Data.List --7.7でsortを使うため
 
 -- 7章のfoldr, foldlの説明は秀逸
 -- foldrは右結合
@@ -49,3 +50,48 @@ transmit :: String -> String
 transmit = decode . channel . encode 
 channel :: [Bit] -> [Bit]
 channel = id
+
+-- 7.7 投票アルゴリズム
+votes :: [String]
+votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
+
+count :: Eq a => a -> [a] -> Int
+count x = length . filter (== x)
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) = x : rmdups (filter (/= x) xs)
+
+result :: (Ord a) => [a] -> [(Int, a)]
+result vs = sort [(count v vs, v) | v <- rmdups vs]
+
+winner :: Ord a => [a] -> a
+winner = snd . last . result
+
+ballots :: [[String]]
+ballots = [["Red", "Green"]
+          ,["Blue"]
+          ,["Green", "Red", "Blue"]
+          ,["Blue", "Green", "Red"]
+          ,["Green"]]
+
+rmempty :: Eq a => [[a]] -> [[a]]
+rmempty = filter (/=[])
+
+elim :: Eq a => a -> [[a]] -> [[a]]
+elim x = map (filter (/= x))
+
+rank :: Ord a => [[a]] -> [a]
+rank = map snd . result . map head 
+
+winner' :: Ord a => [[a]] -> a
+winner' bs = case rank (rmempty bs) of
+            [c] -> c
+            (c:cs) -> winner' $ elim c bs
+
+-- ex01
+-- map f . filter p
+
+-- ex04
+dec2int :: [Int] -> Int 
+dec2int = foldl (\b a -> 10*b + a) 0
