f40809c6321d4810f439584fd8d7358c650b16df
koplec <koplec@github.com>
2024-06-17 23:00:52 +0900

    chap15おわり

--- programming-haskell-2nd-ed/chap15.hs
+++ programming-haskell-2nd-ed/chap15.hs
@@ -13,8 +13,12 @@ inc n = n + 1
 -- 正格
 
 -- 15.2.1 ラムダ式
+-- 関数に対して許される唯一の操作は、関数を引数に適用すること
+-- 関数の本体における、簡約は、引数の適用後
 mult :: Int -> Int -> Int 
 mult x = \y -> x * y
+
+-- 15.3 停止性
 
 
 -- 15.4 簡約の回数
@@ -27,3 +31,20 @@ ones = 1 : ones
 
 -- 15.6 部品プログラム
 -- 遅延評価は、計算の際にデータから制御を切り離す
+-- replicateの例が、「制御とデータを単一の関数として組み合わせて」と説明しているところ
+
+-- エラトステネスの篩
+primes :: [Int]
+primes = sieve [2..]
+
+sieve :: [Int] -> [Int]
+sieve (p:xs) = p : sieve [x | x <- xs, x `mod` p /= 0]
+
+-- 15.7 正格適用
+sumwith :: Int -> [Int] -> Int 
+sumwith v [] = v 
+sumwith v (x:xs) = sumwith (v+x) xs --vは蓄積変数、accumulator 遅延評価だったら、スタックに積まれそう
+
+sumwith2 :: Int -> [Int] -> Int 
+sumwith2 v [] = v
+sumwith2 v (x:xs) = (sumwith2 $! (v+x)) xs --正格評価
